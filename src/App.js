fb93b99545ba0e058419aff7aabf28c141bd6e08
Pranay <pranaydwivedi32@gmail.com>
2023-05-13 03:27:04 +0530

    Navigation bar completed
    Header component and navigation bar completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Header from "./Components/Header/Header.component";
+import CryptoConverter from "./Pages/CryptoConverter/CryptoConverter.component";
+import Homepage from "./Pages/Homepage/Homepage.component";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" Component={Homepage} exact />
+          <Route path="/cryptoconverter" Component={CryptoConverter} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
---
+++ src/Pages/CryptoConverter/CryptoConverter.component.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function CryptoConverter() {
+  return <div>CryptoConverter</div>;
+}
+
+export default CryptoConverter;
