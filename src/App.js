dbdb7e53ef5744380fc360c229af82bc7ca9b428
Pranay <pranaydwivedi32@gmail.com>
2023-05-14 05:00:58 +0530

    added lazy loading for perfomance
    added lazy loading for crypto conversion

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
+import { LinearProgress } from "@mui/material";
+import { Suspense, lazy } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Header from "./Components/Header/Header.component";
-import CryptoConverter from "./Pages/CryptoConverter/CryptoConverter.component";
+// import CryptoConverter from "./Pages/CryptoConverter/CryptoConverter.component";
 import Homepage from "./Pages/Homepage/Homepage.component";
+// Lazy loading CryptoConverter component
+const CryptoConverter = lazy(() =>
+  import("./Pages/CryptoConverter/CryptoConverter.component")
+);
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Header />
-        <Routes>
-          <Route path="/" Component={Homepage} exact />
-          <Route path="/cryptoconverter" Component={CryptoConverter} />
-        </Routes>
+        <Suspense fallback={<LinearProgress />}>
+          <Routes>
+            <Route path="/" Component={Homepage} exact />
+            <Route path="/cryptoconverter" Component={CryptoConverter} />
+            <Route path="*" component={Homepage} />
+          </Routes>
+        </Suspense>
       </BrowserRouter>
     </>
   );
